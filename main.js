c1d6001f714002974716abedc50de65e90b114b8
j4jackie <jackie_duclos@dai.com>
2018-09-15 11:31:08 -0400

    adding my JavaScript file

---
+++ main.js
@@ -0,0 +1,69 @@
+console.log("hello world");
+
+let box = "street"
+console.log(box)
+
+box = "phone";
+console.log(box);
+box = "wire"
+console.log(box);
+
+let city = "Somerville, NJ"
+console.log(city);
+console.log("city=" +city);
+
+city = "Washington, DC";
+console.log(city);
+
+console.log ("city="+ city);
+
+let x = true;
+let m = false;
+console.log(x && !m);
+
+let s = (1 > 0);
+console.log(s);
+
+let itRainsInDC = true;
+
+if (itRainsInDC) {
+    console.log ("pack a raincoat");
+}
+
+console.log(!x && m);
+
+console.log (!x || x);
+
+let languageA = "JavaScript";
+let languageB = "Java";
+
+//this is our boolean
+
+let areTheyEqual = (languageA == languageB);
+console.log(areTheyEqual);
+
+let a = 4;
+
+console.log(a == "4");
+
+//string equality
+console.log(a === "4");
+
+
+let n = 45;
+
+if (n < 45 || n >=46){
+    console.log("will it print?");
+}
+
+let aColor = "red";
+
+if ((3 > (3-1)) && (aColor != "silver") && (aColor == "re"+"d"))
+{
+    console.log("did we make it?");
+}
+
+
+
+
+
